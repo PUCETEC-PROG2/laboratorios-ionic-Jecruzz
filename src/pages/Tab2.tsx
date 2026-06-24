f6d5f0dc93448b6c31c21e6bfc66ab94ede905e4
Usuario invitado <Guest@iMacdePCEA10805.PUCE.EDU.EC>
2026-06-24 16:45:34 -0500

    lab 9

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,7 +1,34 @@
 import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
+import { useHistory } from 'react-router';
+import { useState } from 'react';
+import { Repository } from '../interfaces/Repository';
+import { RepositoryPayload } from '../interfaces/RepositoryPayload';
+import { createRepository } from '../services/GithubService';
 
 const Tab2: React.FC = () => {
+  const history = useHistory();
+  const [repositoryData, setRepositoryData] = useState<RepositoryPayload>({
+    name: "",
+    description: ""
+  })
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMSg] = useState("");
+
+  const saveRepo = async() => {
+    if (repositoryData.name.trim() === '') {
+      setErrorMSg("El nombre del repo es obligatorio");
+      return;
+    }
+    setLoading(true);
+    createRepository(repositoryData)
+    .then(() => hisstory.push("/tab1"))
+    .catch((error) => setErrorMSg("Error al crear los repos :c") + error)
+    .finally(() => setLoading(false));
+  }
+
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -22,12 +49,16 @@ const Tab2: React.FC = () => {
             label='Nombre del Repositorio'
             labelPlacement='floating'
             placeholder="Ingrese el nombre del repositorio"
+            value={repositoryData.name}
+            onIonChange={(e) => setRepositoryData({...repositoryData, name: e.detail.value!})}
           />
           <IonTextarea
             className="form-field"
             label='Descripción del Repositorio'
             labelPlacement='floating'
             placeholder="Ingrese la descripción del repositorio"
+            value={repositoryData.description}
+            onIonChange={(e) => setRepositoryData({...repositoryData, description: e.detail.value!})}
             rows={5}
           />
           <IonButton
